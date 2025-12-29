df84e86afbdf2961638c5a24214f9524c1a37ada
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-12-29 09:09:11 +0100

    refactor(schemas): update phone validation regex for Czech

--- src/schemas/contact_schemas.py
+++ src/schemas/contact_schemas.py
@@ -15,7 +15,7 @@ class ContactCreate(BaseModel):
     first_name: str = Field(max_length=50, min_length=2)
     last_name: str = Field(max_length=50, min_length=2)
     email: EmailStr = Field(max_length=100)
-    phone_number: str = Field(pattern=r"^(\+38)?\d{10}$")
+    phone_number: str = Field(pattern=r"^(\+420)?\d{9}$")
     date_of_birth: date
     additional_info: str | None = Field(default=None, max_length=100)
 
